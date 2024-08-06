529e40b4c6160e260da7dbf58d3a48858f91fe1a
Suzy Kim <skme1103@naver.com>
2024-08-06 16:27:03 +0900

    End promise lecture

--- src/theory/async/promise.js
+++ src/theory/async/promise.js
@@ -8,4 +8,25 @@
 // when new Promise is created, the executor runs automatically.
 const promise = new Promise((resolve, reject) => {
     // doing some heavy work(network, file)
-})
+    console.log('doing somthing...');
+    setTimeout(() => {
+        // resolve('ellie');
+        reject(new Error('no network'));
+    }, 2000);
+});
+
+//2. Consumer : then, catch, finally
+promise
+    .then(value => {
+        console.log(value);
+    })
+    .catch(error => {
+        console.log(value);
+    })
+    .finally(() => {
+        console.log('finally')
+    })
+
+
+    //3. promise chaining
+    
